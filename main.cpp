bb159a8cfb7c0c8e5061b7b4664d190859939759
Sascha.Schneider <sascha.schneider@exacom.tech>
2023-03-23 22:04:38 +0100

    Can set all application volumes to 1.0.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <audioclient.h>
 #include <mmdeviceapi.h>
 #include <Functiondiscoverykeys_devpkey.h>
+
+#include <audiopolicy.h>
 #include <iostream>
+#include <mmeapi.h>
+#include <string>
 
 //-----------------------------------------------------------
 // This function enumerates all active (plugged in) audio
@@ -33,7 +37,7 @@ void PrintEndpointNames()
   EXIT_ON_ERROR(hr)
 
   hr = pEnumerator->EnumAudioEndpoints(
-          eRender, DEVICE_STATE_ACTIVE | DEVICE_STATE_UNPLUGGED,
+          eAll, DEVICE_STATE_ACTIVE | DEVICE_STATE_UNPLUGGED,
           &pCollection);
   EXIT_ON_ERROR(hr)
 
@@ -95,10 +99,65 @@ Exit:
   SAFE_RELEASE(pProps)
 }
 
+void changeFGWindowVolume()
+{
+  IMMDevice *mmDevice;
+  IMMDeviceEnumerator *mmDeviceEnum;
+  IAudioSessionManager2 *sessionManager;
+  IAudioSessionEnumerator *sessionEnum;
+  IAudioSessionControl *sessionControl;
+  IAudioSessionControl2 *sessionControl2;
+  ISimpleAudioVolume *audioVolume;
+
+  CoCreateInstance(__uuidof(MMDeviceEnumerator), 0, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&mmDeviceEnum);
+  mmDeviceEnum->GetDefaultAudioEndpoint(eRender, eMultimedia, &mmDevice);
+  mmDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, 0, (void**)&sessionManager);
+  sessionManager->GetSessionEnumerator(&sessionEnum);
+
+  //DWORD activePid;
+  //GetWindowThreadProcessId(activeHWND, &activePid);
+
+  int sessionCount;
+  sessionEnum->GetCount(&sessionCount);
+  for (int i = 0; i < sessionCount; i++)
+  {
+    sessionEnum->GetSession(i, &sessionControl);
+    //sessionControl->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&sessionControl2);
+
+    LPWSTR name = NULL;
+    sessionControl->GetDisplayName(&name);
+    std::wcout << "sessionname: " << name << std::endl;
+
+
+    sessionControl->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&audioVolume);
+
+    BOOL muted;
+    audioVolume->GetMute(&muted);
+
+    float volumeLevel;
+    audioVolume->GetMasterVolume(&volumeLevel);
+
+    std::cout << std::to_string(volumeLevel) << std::endl;
+
+    float newVolumeLevel = 33.0 / 100.0f;
+    audioVolume->SetMasterVolume(1.0f, 0);
+    audioVolume->Release();
+
+    sessionControl->Release();
+    //sessionControl2->Release();
+  }
+
+  sessionEnum->Release();
+  sessionManager->Release();
+  mmDevice->Release();
+  mmDeviceEnum->Release();
+}
+
 int main() {
   CoInitialize(nullptr);
 
   std::cout << "Hello, World!" << std::endl;
   PrintEndpointNames();
+  changeFGWindowVolume();
   return 0;
 }
