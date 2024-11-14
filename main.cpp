4edc6812d748b61016843dcb7beecb8b584930ec
astralaster <schneider.sascha@mein.gmx>
2024-11-14 15:15:13 +0100

    Fix memory leak.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,20 @@
 #include <audioclient.h>
 #include <mmdeviceapi.h>
-#include <Functiondiscoverykeys_devpkey.h>
 #include <audiopolicy.h>
 #include <endpointvolume.h>
 
 #include <iostream>
-#include <mmeapi.h>
 #include <string>
-#include <cwchar>
 #include <thread>
 
 static float old_volume = 0.0f;
 static int sessionCountOld = 0;
 
-void syncMasterVolumeToAllApps()
+void syncMasterVolumeToAllApps(IAudioEndpointVolume *endpointVolume, IAudioSessionEnumerator *sessionEnum)
 {
-  IMMDevice *mmDevice;
-  IMMDeviceEnumerator *mmDeviceEnum;
-  IAudioSessionManager2 *sessionManager;
-  IAudioSessionEnumerator *sessionEnum;
-  IAudioSessionControl *sessionControl;
-  ISimpleAudioVolume *audioVolume;
-  IAudioEndpointVolume *endpointVolume;
-
-  CoCreateInstance(__uuidof(MMDeviceEnumerator), 0, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&mmDeviceEnum);
-  mmDeviceEnum->GetDefaultAudioEndpoint(eRender, eMultimedia, &mmDevice);
-
-  mmDevice->Activate(__uuidof(IAudioEndpointVolume) , CLSCTX_ALL, 0, (void**)&endpointVolume);
   float volume = 0.0f;
   endpointVolume->GetChannelVolumeLevelScalar(0, &volume);
 
-  mmDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, 0, (void **) &sessionManager);
-  sessionManager->GetSessionEnumerator(&sessionEnum);
   int sessionCount;
   sessionEnum->GetCount(&sessionCount);
 
@@ -41,6 +24,10 @@ void syncMasterVolumeToAllApps()
 
     for (int i = 0; i < sessionCount; i++)
     {
+      IAudioSessionControl *sessionControl;
+      ISimpleAudioVolume *audioVolume;
+
+
       sessionEnum->GetSession(i, &sessionControl);
       sessionControl->QueryInterface(__uuidof(ISimpleAudioVolume), (void **) &audioVolume);
 
@@ -50,24 +37,33 @@ void syncMasterVolumeToAllApps()
       sessionControl->Release();
     }
 
-    sessionEnum->Release();
-    sessionManager->Release();
     old_volume = volume;
+    sessionCountOld = sessionCount;
   }
-
-  sessionCountOld = sessionCount;
-  endpointVolume->Release();
-  mmDevice->Release();
-  mmDeviceEnum->Release();
 }
 
 int main() {
   CoInitialize(nullptr);
 
-  std::cout << "Volsync v1.1" << std::endl;
+  std::cout << "Volsync v1.2" << std::endl;
+
+  IMMDevice *mmDevice;
+  IMMDeviceEnumerator *mmDeviceEnum;
+  IAudioSessionManager2 *sessionManager;
+  IAudioSessionEnumerator *sessionEnum;
+  IAudioEndpointVolume *endpointVolume;
+
+  CoCreateInstance(__uuidof(MMDeviceEnumerator), 0, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&mmDeviceEnum);
+  mmDeviceEnum->GetDefaultAudioEndpoint(eRender, eMultimedia, &mmDevice);
+
+  mmDevice->Activate(__uuidof(IAudioEndpointVolume) , CLSCTX_ALL, 0, (void**)&endpointVolume);
+  mmDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, 0, (void**)&sessionManager);
+
   while(true)
   {
-    syncMasterVolumeToAllApps();
+    sessionManager->GetSessionEnumerator(&sessionEnum);
+    syncMasterVolumeToAllApps(endpointVolume, sessionEnum);
+    sessionEnum->Release();
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(100ms);
   }
